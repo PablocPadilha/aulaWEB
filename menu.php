b178f6a008265aaf225bb080377a5ccc7e201c35
Your Name <you@example.com>
2019-04-05 20:08:32 -0300

    Carga inicial

---
+++ menu.php
@@ -0,0 +1,30 @@
+<?php include './validaSessao.php'; ?>
+
+<div class = "row">
+    <nav class = "navbar">
+
+        <form name="crono" menu = "action" method = "POST">
+            <div class = " navbar-brand ">
+                <!--## Código antigo ##   
+                <button name = "CLIENTES"><a href = "index.php"></a>CLIENTES</button>
+                <button name = "PRODUTOS"><a href = "index.php"></a>PRODUTOS</button>
+                <button name = "PEDIDOS"><a href = "index.php">PEDIDOS</a></button>
+                <button name = "SAIR"><a href = "index.php">SAIR</a></button> -->
+
+                <input type = "submit" class = "btn btn-primary" name = "CLIENTES" value = "CLIENTES">
+                <input type = "submit" class = "btn btn-primary" name = "PRODUTOS" value = "PRODUTOS">
+                <input type = "submit" class = "btn btn-primary" name = "PEDIDOS" value = "PEDIDOS">
+                <input type = "submit" class = "btn btn-primary" name = "SAIR" value = "SAIR">
+            </div> </form>
+        
+    </nav>
+</div>
+
+
+
+
+
+
+
+
+
